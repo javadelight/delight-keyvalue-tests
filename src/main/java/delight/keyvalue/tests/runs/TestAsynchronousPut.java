aa903ff3ed5fa3b1b6092752ac91b466bb3beae7
mxro <mxro@nowhere.com>
2015-07-11 15:40:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 15:40:16 EST 2015

--- src/main/java/delight/keyvalue/tests/runs/TestAsynchronousPut.java
+++ src/main/java/delight/keyvalue/tests/runs/TestAsynchronousPut.java
@@ -1,5 +1,24 @@
 package delight.keyvalue.tests.runs;
 
+import delight.concurrency.jre.JreConcurrency;
+import delight.functional.Function;
+import delight.keyvalue.Store;
+import delight.keyvalue.Stores;
+import delight.keyvalue.tests.StoreTests;
+import org.junit.Test;
+
 @SuppressWarnings("all")
 public class TestAsynchronousPut {
+  @Test
+  public void test() {
+    final Function<Void, Store<String, Object>> _function = new Function<Void, Store<String, Object>>() {
+      @Override
+      public Store<String, Object> apply(final Void it) {
+        JreConcurrency _jreConcurrency = new JreConcurrency();
+        Store<String, Object> _hashMap = Stores.<String, Object>hashMap();
+        return Stores.<String, Object>enforceAsynchronousPut(10, _jreConcurrency, _hashMap);
+      }
+    };
+    StoreTests.testAll(_function);
+  }
 }
--- src/main/xtend/delight/keyvalue/tests/runs/TestAsynchronousPut.xtend
+++ src/main/xtend/delight/keyvalue/tests/runs/TestAsynchronousPut.xtend
@@ -1,5 +1,16 @@
 package delight.keyvalue.tests.runs
 
+import delight.concurrency.jre.JreConcurrency
+import delight.keyvalue.Stores
+import delight.keyvalue.tests.StoreTests
+import org.junit.Test
+
 class TestAsynchronousPut {
-	
+	@Test
+	def void test() {
+		StoreTests.testAll [
+			 Stores.enforceAsynchronousPut(10, new JreConcurrency(),
+			Stores.hashMap);
+		]
+	}
 }
