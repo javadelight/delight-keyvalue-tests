ea9669b7fc9053aee75a9ec3230d24b2cb2e46c6
mxro <mxro@nowhere.com>
2015-07-11 15:32:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 15:32:16 EST 2015

--- src/main/java/delight/keyvalue/tests/StoreTests.java
+++ src/main/java/delight/keyvalue/tests/StoreTests.java
@@ -1,5 +1,13 @@
 package delight.keyvalue.tests;
 
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.async.jre.Async;
+import delight.functional.Function;
+import delight.functional.Success;
+import delight.keyvalue.Store;
 import delight.keyvalue.tests.StoreTest;
 import delight.keyvalue.tests.TestMultiDelete;
 import delight.keyvalue.tests.TestMultiSelect;
@@ -11,6 +19,32 @@ import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 
 @SuppressWarnings("all")
 public class StoreTests {
+  public static void testAll(final Function<Void, Store<String, Object>> factory) {
+    List<StoreTest> _all = StoreTests.all();
+    for (final StoreTest test : _all) {
+      {
+        final Store<String, Object> store = factory.apply(null);
+        final Operation<Success> _function = new Operation<Success>() {
+          @Override
+          public void apply(final ValueCallback<Success> callback) {
+            SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+            store.start(_asSimpleCallback);
+          }
+        };
+        Async.<Success>waitFor(_function);
+        test.test(store);
+        final Operation<Success> _function_1 = new Operation<Success>() {
+          @Override
+          public void apply(final ValueCallback<Success> callback) {
+            SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+            store.stop(_asSimpleCallback);
+          }
+        };
+        Async.<Success>waitFor(_function_1);
+      }
+    }
+  }
+  
   public static List<StoreTest> all() {
     TestMultiDelete _testMultiDelete = new TestMultiDelete();
     TestMultiSelect _testMultiSelect = new TestMultiSelect();
--- src/main/xtend/delight/keyvalue/tests/StoreTests.xtend
+++ src/main/xtend/delight/keyvalue/tests/StoreTests.xtend
@@ -1,9 +1,28 @@
 package delight.keyvalue.tests
 
+import delight.async.AsyncCommon
+import delight.async.jre.Async
+import delight.functional.Function
+import delight.keyvalue.Store
 import java.util.List
 
 class StoreTests {
-	
+
+	def static void testAll(Function<Void, Store<String, Object>> factory) {
+		for (test : all()) {
+			val store = factory.apply(null)
+			Async.waitFor [ callback |
+				store.start(AsyncCommon.asSimpleCallback(callback));
+			]
+
+			test.test(store)
+
+			Async.waitFor [ callback |
+				store.stop(AsyncCommon.asSimpleCallback(callback));
+			]
+		}
+	}
+
 	def static List<StoreTest> all() {
 		return #[
 			new TestMultiDelete,
@@ -12,5 +31,5 @@ class StoreTests {
 			new TestThatParellelWorkerProcessesPuts
 		]
 	}
-	
+
 }
