e94d4e24653f50f463c999c80992aa5df17cdec8
mxro <mxro@nowhere.com>
2015-07-11 15:35:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 15:35:16 EST 2015

--- src/main/java/delight/keyvalue/tests/StoreTests.java
+++ src/main/java/delight/keyvalue/tests/StoreTests.java
@@ -10,8 +10,8 @@ import delight.functional.Success;
 import delight.keyvalue.Store;
 import delight.keyvalue.tests.DefMultiDelete;
 import delight.keyvalue.tests.DefMultiSelect;
-import delight.keyvalue.tests.StoreTest;
 import delight.keyvalue.tests.DefPut;
+import delight.keyvalue.tests.StoreTest;
 import delight.keyvalue.tests.TestThatParellelWorkerProcessesPuts;
 import java.util.Collections;
 import java.util.List;
@@ -48,8 +48,8 @@ public class StoreTests {
   public static List<StoreTest> all() {
     DefMultiDelete _defMultiDelete = new DefMultiDelete();
     DefMultiSelect _defMultiSelect = new DefMultiSelect();
-    DefPut _testThatAsynchronousPutMapCanBeStopped = new DefPut();
+    DefPut _defPut = new DefPut();
     TestThatParellelWorkerProcessesPuts _testThatParellelWorkerProcessesPuts = new TestThatParellelWorkerProcessesPuts();
-    return Collections.<StoreTest>unmodifiableList(CollectionLiterals.<StoreTest>newArrayList(_defMultiDelete, _defMultiSelect, _testThatAsynchronousPutMapCanBeStopped, _testThatParellelWorkerProcessesPuts));
+    return Collections.<StoreTest>unmodifiableList(CollectionLiterals.<StoreTest>newArrayList(_defMultiDelete, _defMultiSelect, _defPut, _testThatParellelWorkerProcessesPuts));
   }
 }
